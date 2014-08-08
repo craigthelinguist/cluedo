6b2c6288976763b42f3e2f5f28f014196d020cf1
craigthelinguist <sinroth@gmail.com>
2014-08-08 20:50:25 +1200

    renamed accusation -> suggestion

--- src/game/Suggestion.java
+++ src/game/Suggestion.java
@@ -3,20 +3,20 @@ package game;
 import cards.Room;
 import cards.Weapon;
 
-public class Accusation {
+public class Suggestion {
 
 	private Room room;
 	private Character character;
 	private Weapon weapon;
 
 	/**
-	 * An accusation equals another if the cards comprising them are the same.
-	 * @return: whether two accusations are the same.
+	 * An Suggestion equals another if the cards comprising them are the same.
+	 * @return: whether two Suggestions are the same.
 	 */
 	public boolean equals(Object other){
 		if (other == null) return false;
-		if (!(other instanceof Accusation)) return false;
-		Accusation acc = (Accusation)other;
+		if (!(other instanceof Suggestion)) return false;
+		Suggestion acc = (Suggestion)other;
 		return room==acc.room && character==acc.character && weapon==acc.weapon;
 	}
 
