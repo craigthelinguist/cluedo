fd49dc3b5328f786277ee8d636f855b51cfc37bc
Aaron Craig <craigaaro@steamboat.ecs.vuw.ac.nz>
2014-08-13 13:02:51 +1200

    starting to do dealing cards

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,18 +1,22 @@
 package game;
 
 import java.awt.Image;
+import java.util.LinkedList;
 
+import cards.Card;
 import cards.Person;
 
 public class Player {
 
 	private final Person avatar;
 	private Tile location;
-	
+	private LinkedList<Card> cards;
+
 	public Player(Person p){
 		avatar = p;
+		cards = new LinkedList<>();
 	}
-	
+
 	/**
 	 * Return tile this player is standing on.
 	 * @return
@@ -20,7 +24,7 @@ public class Player {
 	public Tile getLocation(){
 		return location;
 	}
-	
+
 	/**
 	 * Return the image representing this player on the board.
 	 * @return
@@ -28,18 +32,20 @@ public class Player {
 	public Image getAvatar(){
 		return avatar.getTokenImage();
 	}
-	
+
 	/**
 	 * Return the portrait representing this player in the GamePanel.
 	 */
 	public Image getPortrait(){
 		return avatar.getPortraitImage();
 	}
-	
+
+	private void
+
 	public void setLocation(Tile tile){
 		location = tile;
 	}
-	
+
 	public String toString(){
 		return avatar.toString();
 	}
--- src/gui/GameFrame.java
+++ src/gui/GameFrame.java
@@ -56,7 +56,7 @@ public class GameFrame extends JFrame {
 
 		    }
 
-		});
+			});
 
 		//this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
