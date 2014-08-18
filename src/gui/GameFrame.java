01d9c197165755f906a1068688a5ed3a43453881
EwanMoshi <moshiewan@live.com>
2014-08-18 19:01:04 +1200

    got rid of talking scarlett

--- src/gui/GameFrame.java
+++ src/gui/GameFrame.java
@@ -19,7 +19,6 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.WindowConstants;
 
-import Talking.MissScarlett;
 import main.Constants;
 import cards.Card;
 import cards.Person;
@@ -111,10 +110,6 @@ public class GameFrame extends JFrame {
 	protected void buttonPressed(String button){
 
 		if (button.equals("End Turn")) {
-			Player[] players = board.getPlayers();
-			if(players[board.getPlayerInt()].avatar.toString().equalsIgnoreCase("Miss Scarlett")) {
-				new MissScarlett(this,board);
-			}
 			board.endTurn();
 		}
 		else if (button.equals("Roll Dice")){
