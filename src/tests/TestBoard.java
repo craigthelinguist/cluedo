36790ce6639f18540dd44fa3968937622bc3d84d
EwanMoshi <moshiewan@live.com>
2014-08-18 22:25:31 +1200

    added tests for board

---
+++ src/tests/TestBoard.java
@@ -0,0 +1,59 @@
+package tests;
+
+import game.Board;
+import game.Player;
+import game.Tile;
+import game.Tile.Terrain;
+
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+import org.junit.Test;
+
+import cards.Person;
+import cards.Room;
+
+public class TestBoard {
+
+	private Board board;
+	
+	/*Create a new board*/
+	@Test public void createNewBoard()   {
+		createBoard();
+	}
+	@Test public void endingTurn() {
+		Board b = createBoard();
+		Player previousPlayer = b.getCurrentPlayer();
+		b.endTurn(); //end turn
+		if(!(b.getCurrentPlayer().toString().equalsIgnoreCase(previousPlayer.toString()))) { //check to see if it's a new player's turn
+			System.out.println("Successfully changed player turn");
+		}
+	}
+	
+	@Test public void movePlayer() {
+		Board b = createBoard();
+		Player p = b.getCurrentPlayer();
+		Tile previousTile = p.getLocation();
+		boolean[] neighbours = {true,true,true,true};
+		b.movePlayer(Tile.makePassableTile(5, 5, neighbours, null));
+		Tile currentTile = p.getLocation();
+		if(previousTile != currentTile) {
+			System.out.println("Moving Player test passed");
+		}
+	}
+	
+	/**
+	 * Helper method creates new board
+	 * @return
+	 */
+	public Board createBoard() {
+			try {
+				Player[] playerArray = {new Player(new Person("scarlett")), new Player(new Person("mustard")), new Player(new Person("white"))};
+				board = new Board(playerArray);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return board;
+	}
+}
